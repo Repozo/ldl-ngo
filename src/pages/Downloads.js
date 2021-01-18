96497bae60d1099f4470dcc0d40cc20b0f08dfba
akshat jain <akshat.gms@gmail.com>
2021-01-18 14:27:44 +0530

    Added Download Page

--- src/pages/Downloads.js
+++ src/pages/Downloads.js
@@ -16,24 +16,24 @@ function Downloads() {
   </tr>
   <tr>
     <td>Financial Year: 2014-2016 Balance Sheet</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1016/z" target="_blank" >Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1016/z" target="_blank" rel="noreferrer" >Download</a></td>
     
   </tr>
   <tr>
   <td>Financial Year: 2016-2017 Balance Sheet</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1019/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1019/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   <tr>
   <td>Financial Year: 2017-2018 Balance Sheet</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1022/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1022/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   <tr>
   <td>Financial Year: 2018-2019 Balance Sheet</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1988/"target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1988/"target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   <tr>
   <td>Financial Year: 2019-2020 Balance Sheet</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1985/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1985/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   
 </table>
@@ -50,28 +50,28 @@ function Downloads() {
   </tr>
   <tr>
     <td>Registration Certificate</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/873/" target="_blank" >Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/873/" target="_blank" rel="noreferrer">Download</a></td>
     
   </tr>
   <tr>
   <td>PAN Card</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/866/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/866/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   <tr>
   <td>Internal Report 2019-2020</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1997/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1997/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   <tr>
   <td>Internal Report 2018-2019</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1994/"target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1994/"target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   <tr>
   <td>Internal Report 2017-2018</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1281/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1281/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   <tr>
   <td>Internal Report 2016-2017</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/877/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/877/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
 </table>
 <br/>
@@ -87,12 +87,12 @@ function Downloads() {
   </tr>
   <tr>
     <td>Certificates issued between 2015-2018</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/891/" target="_blank" >Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/891/" target="_blank" rel="noreferrer">Download</a></td>
     
   </tr>
   <tr>
   <td>Certificates issued in 2019</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1367/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1367/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
   
 </table>
@@ -110,31 +110,31 @@ function Downloads() {
   </tr>
   <tr>
     <td>Audit Report 2015-2016</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1677/" target="_blank" >Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1677/" target="_blank" rel="noreferrer">Download</a></td>
     
  </tr>
   <tr>
   <td>Audit Report 2016-2017</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1682/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1682/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
      
 
   <tr>
   <td>Audit Report 2017-2018</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1684/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/1684/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
      
 
   <tr>
   <td>Audit Report 2018-2019</td>
-    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/2005/" target="_blank">Download</a></td>
+    <td><a className="download-link" href="https://www.lightdeliteracy.com/download/2005/" target="_blank" rel="noreferrer">Download</a></td>
   </tr>
 </table>
 <br/>
         </div>
         <h1 className="download-subhead"> Internship Certificates</h1>
    
-        <a className="download-link last-link" href="https://drive.google.com/folderview?id=1HDR5RDb6L8xSIJ96fQPCXfdZ08yjOMfy" target="_blank">Download Internship Certificates</a>
+        <a className="download-link last-link" href="https://drive.google.com/folderview?id=1HDR5RDb6L8xSIJ96fQPCXfdZ08yjOMfy" target="_blank" rel="noreferrer">Download Internship Certificates</a>
    <br/>  
    <br/>
     </div>
